3a26656874da43e12968b78ec4d6b2f8ef39e856
Jonah Adeku Ajayi <jaajayi@sidmach.com>
2021-11-17 19:32:10 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Markup from './markup/markup';
+
+// Plugins Stylesheet
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Slick Carousel
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// StyleSheet
+import './css/style.css';
+
+function App() {
+	return (
+		<div className="page-wraper">
+			<Markup />
+		</div>
+	);
+}
+
+export default App;
